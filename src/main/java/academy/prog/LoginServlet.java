78f89134d18432b4c1f9e0fb404952967faf70de
SofiaShein <sheinkinasofiya@gmail.com>
2024-11-10 10:51:28 +0200

    change field to private

--- src/main/java/academy/prog/LoginServlet.java
+++ src/main/java/academy/prog/LoginServlet.java
@@ -4,30 +4,29 @@ import jakarta.servlet.http.*;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
 public class LoginServlet extends HttpServlet {
 
-    ArrayList<User> users = new ArrayList<>();
+    private final ArrayList<User> users = new ArrayList<>();
 
+    public synchronized void addUser(User user) {
+        users.add(user);
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
         String name = request.getParameter("name");
         String lastname = request.getParameter("lastname");
         String age = request.getParameter("age");
         String color = request.getParameter("color");
         String country = request.getParameter("country");
 
-        User user = new User(name,  lastname, age, color, country);
+        User user = new User(name, lastname, age, color, country);
 
-        users.add(user);
+        addUser(user);
 
         request.getSession(true).setAttribute("users", users);
 
         response.sendRedirect("userResponses.jsp");
-
-
     }
 
     @Override
